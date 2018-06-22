5d51b25e99dc794db9993869cd697971c62e8977
snipzer <l.arif@laposte.net>
2018-06-22 13:40:31 +0200

    removing sending notification util echange out is specified

--- src/main/java/main/core/rabbit/Listener.java
+++ src/main/java/main/core/rabbit/Listener.java
@@ -61,7 +61,7 @@ public class Listener {
 
         String payload = this.trackingService.createPayload(trackingTmp);
 
-        System.out.println("Sending notification on exchange: " + this.environment.getProperty(StringUtil.SPRING_RABBITMQ_TEMPLATE_EXCHANGE) + "in queue :" + StringUtil.SPRING_MICRO_SUIVI_OUT);
+        System.out.println("Sending notification on exchange: " + this.environment.getProperty(StringUtil.SPRING_RABBITMQ_TEMPLATE_EXCHANGE) + " in queue :" + StringUtil.SPRING_MICRO_SUIVI_OUT);
 //        rabbitTemplate.convertAndSend(
 //                this.environment.getProperty(StringUtil.SPRING_RABBITMQ_TEMPLATE_EXCHANGE),
 //                StringUtil.SPRING_MICRO_SUIVI_OUT,
